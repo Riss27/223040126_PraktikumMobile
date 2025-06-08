316a3cd1588192e104f11f47b965fa5ed40987a5
Faris <faris.223040126@mail.unpas.ac.id>
2025-06-08 14:00:51 +0700

    progress:step9

--- app/src/main/java/com/example/storyapp/MainActivity.kt
+++ app/src/main/java/com/example/storyapp/MainActivity.kt
@@ -4,31 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.storyapp.ui.navigation.StoryApp
+import dagger.hilt.android.AndroidEntryPoint
 import com.example.storyapp.ui.theme.StoryAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             StoryAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                StoryApp()
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
